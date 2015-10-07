89d3d8f4585c944742928a6c720e36e4f09630ea
Rasmus Selsmark <rasmus@unity3d.com>
2015-10-07 23:34:54 +0300

    Added config panel to allow changing zone ids

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,10 +7,17 @@ public class UIController : MonoBehaviour
 	public UnityEngine.UI.Button ShowDefaultAdButton;
 	public UnityEngine.UI.Button ShowRewardedAdButton;
 	public UnityEngine.UI.Text LogText;
+	public GameObject ConfigPanel;
+	public UnityEngine.UI.InputField DefaultAdZoneIdInput;
+	public UnityEngine.UI.InputField RewardedAdZoneIdInput;
 
 	internal bool AdsInitialized;
 
 	private bool hasShownRewardedAd;  // since we have bug where zone is not reset, so in that case pass zone id
+
+	private const string GameIdPlayerPrefsKey = "GameId";
+	private const string DefaultAdZoneIdPlayerPrefsKey = "DefaultAdZoneId";
+	private const string RewardedAdZoneIdPlayerPrefsKey = "RewardedAdZoneId";
 
 	private static UIController instance = null;
 	public static UIController Instance
@@ -27,9 +34,26 @@ public class UIController : MonoBehaviour
 
 	void Start ()
 	{
-		if (PlayerPrefs.HasKey ("GameId"))
+		ConfigPanel.SetActive (false);
+		if (PlayerPrefs.HasKey (GameIdPlayerPrefsKey))
 		{
-			GameIdInput.text = PlayerPrefs.GetString ("GameId");
+			GameIdInput.text = PlayerPrefs.GetString (GameIdPlayerPrefsKey);
+		}
+
+		if (PlayerPrefs.HasKey (DefaultAdZoneIdPlayerPrefsKey)) {
+			DefaultAdZoneIdInput.text = PlayerPrefs.GetString (DefaultAdZoneIdPlayerPrefsKey);
+		}
+		else
+		{
+			DefaultAdZoneIdInput.text = "defaultZone";
+		}
+
+		if (PlayerPrefs.HasKey (RewardedAdZoneIdPlayerPrefsKey)) {
+			RewardedAdZoneIdInput.text = PlayerPrefs.GetString (RewardedAdZoneIdPlayerPrefsKey);
+		}
+		else
+		{
+			RewardedAdZoneIdInput.text = "rewardedVideoZone";
 		}
 	}
 
@@ -50,13 +74,15 @@ public class UIController : MonoBehaviour
 		LogText.text = string.Format ("{0}\n{1}", text, LogText.text);
 	}
 
-	public void InitializeAds()
+	public void InitializeAds ()
 	{
 		Main.InitializeAds (GameIdInput.text);
-		PlayerPrefs.SetString ("GameId", GameIdInput.text);
+		PlayerPrefs.SetString (GameIdPlayerPrefsKey, GameIdInput.text);
+		PlayerPrefs.SetString (DefaultAdZoneIdPlayerPrefsKey, DefaultAdZoneIdInput.text);
+		PlayerPrefs.SetString (RewardedAdZoneIdPlayerPrefsKey, RewardedAdZoneIdInput.text);
 	}
 
-	public void ShowDefaultAd()
+	public void ShowDefaultAd ()
 	{
 		if (!hasShownRewardedAd)
 		{
@@ -64,17 +90,27 @@ public class UIController : MonoBehaviour
 		}
 		else
 		{
-			Main.ShowAd ("defaultZone");
+			Main.ShowAd (DefaultAdZoneIdInput.text);
 		}
 	}
 
-	public void ShowRewardedAd()
+	public void ShowRewardedAd ()
 	{
 		hasShownRewardedAd = true;
-		Main.ShowAd ("rewardedVideoZone");
+		Main.ShowAd (RewardedAdZoneIdInput.text);
 	}
 
-	public void Quit()
+	public void ShowConfig ()
+	{
+		ConfigPanel.SetActive (true);
+	}
+
+	public void HideConfig ()
+	{
+		ConfigPanel.SetActive (false);
+	}
+
+	public void Quit ()
 	{
 		Application.Quit ();
 	}

03f49ca252ad1a77b292d6aa84e065003334091f
Rasmus Selsmark <rasmus@unity3d.com>
2015-10-07 16:18:39 +0300

    Added scripts for showing ads

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Main : MonoBehaviour
+{
+	void Update ()
+	{
+		UIController ui = UIController.Instance;
+
+		if (ui)
+		{
+			ui.AdsInitialized = Advertisement.isInitialized;
+		}
+	}
+
+	public static void InitializeAds (string gameId)
+	{
+		if (!Advertisement.isSupported)
+		{
+			Log ("Ads not supported on this platform");
+			return;
+		}
+
+		if ((gameId == null) || (gameId.Trim ().Length == 0))
+		{
+			Log ("Please provide a game id");
+			return;
+		}
+
+		Log (string.Format ("Initializing ads for game id {0}", gameId));
+		Advertisement.Initialize (gameId);
+	}
+
+	public static void ShowAd ()
+	{
+		ShowAd (null);
+	}
+
+	public static void ShowAd (string zoneId)
+	{
+		if (!Advertisement.isInitialized)
+		{
+			Log ("Ads hasn't been initialized yet. Cannot show ad");
+			return;
+		}
+
+		if (!Advertisement.IsReady (zoneId))
+		{
+			if (zoneId == null)
+			{
+				Log ("Ads not ready for default zone. Please wait a few seconds and try again");
+			}
+			else
+			{
+				Log (string.Format("Ads not ready for zone '{0}'. Please wait a few seconds and try again", zoneId));
+			}
+
+			return;
+		}
+
+		ShowOptions options = new ShowOptions
+		{
+			resultCallback = ShowAdResultCallback
+		};
+		Advertisement.Show (zoneId, options);
+	}
+
+	private static void ShowAdResultCallback(ShowResult result)
+	{
+		Log (string.Format ("Ad completed with result: {0}", result));
+	}
+
+	public static void Log(string message)
+	{
+		string text = String.Format ("{0:HH:mm:ss} {1}", DateTime.Now, message);
+		Debug.Log ("=== " + text + " ===");
+
+		UIController ui = UIController.Instance;
+		if (ui)
+		{
+			ui.Log (text);
+		}
+	}
+}
+
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+	public UnityEngine.UI.Text GameIdInput;
+	public UnityEngine.UI.Button InitializeButton;
+	public UnityEngine.UI.Button ShowDefaultAdButton;
+	public UnityEngine.UI.Button ShowRewardedAdButton;
+	public UnityEngine.UI.Text LogText;
+
+	internal bool AdsInitialized;
+
+	private bool hasShownRewardedAd;  // since we have bug where zone is not reset, so in that case pass zone id
+
+	private static UIController instance = null;
+	public static UIController Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<UIController>();
+			}
+			return instance;
+		}
+	}
+
+	void Update()
+	{
+		UpdateUI ();
+	}
+	
+	public void UpdateUI ()
+	{
+		InitializeButton.interactable = !AdsInitialized;
+		ShowDefaultAdButton.interactable = AdsInitialized;
+		ShowRewardedAdButton.interactable = AdsInitialized;
+	}
+
+	public void Log (string text)
+	{
+		LogText.text = string.Format ("{0}\n{1}", text, LogText.text);
+	}
+
+	public void InitializeAds()
+	{
+		Main.InitializeAds (GameIdInput.text);
+	}
+
+	public void ShowDefaultAd()
+	{
+		if (!hasShownRewardedAd)
+		{
+			Main.ShowAd ();  // we want to make sure this also works, as game devs might typically show ads this way
+		}
+		else
+		{
+			Main.ShowAd ("defaultZone");
+		}
+	}
+
+	public void ShowRewardedAd()
+	{
+		hasShownRewardedAd = true;
+		Main.ShowAd ("rewardedVideoZone");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
